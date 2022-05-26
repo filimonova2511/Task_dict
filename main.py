d1c79a29ff8160360b7f118fdbeb2817d8b6aef4
Yengeniya Filimonova <filimonova25-11@mail.ru>
2022-05-26 16:15:30 +0300

    add answers 1-3 to main.py

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ my_list = [42, 43]
 my_dict = {'foo': {'a': 12, 'b': (1, 2, 3, 4, my_list)},
 'bar': {'c': 12, 'd': {5, 6, 7, 8}},
 'moo': {'e': 12, 'f': {'Lol': ['L', 'o', 'l']}}}
+
+# 1 Значение ключа 'foo'
+print(my_dict['foo'])
+
+# 2 Значение ключа 'b'
+print(my_dict['foo']['b'])
+
+# 3 Добавить в my_list 44
+my_list.append(44)
+print(my_list)
